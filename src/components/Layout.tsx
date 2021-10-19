26594a2591c7d798325bb75feb65b6ed6bb75c0b
njihiamark <njihiamark@gmail.com>
2021-10-20 01:43:59 +0300

    positioning of dark patterns

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -17,6 +17,10 @@ const Layout = ({ children, title = 'Quiz app', theme = "dark" }: Props) => (
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
     </Head>
     <div className={styles["app"]+` ${theme === "light" ? styles["app--light"] : styles["app--dark"]}`}>
+        <div className={styles["pattern1"]}>pattern1</div>
+        <div className={styles["pattern2"]}>pattern2</div>
+        <div className={styles["pattern3"]}>pattern3</div>
+        <div className={styles["pattern4"]}>pattern4</div>
         <div className="container">
             {children}
         </div>
