81c4be5c21693572a0247d9d7950ab801a403190
njihiamark <njihiamark@gmail.com>
2021-10-20 02:12:30 +0300

    use BEM for the patterns

--- src/pages/welcome.tsx
+++ src/pages/welcome.tsx
@@ -4,7 +4,7 @@ import Layout from '../components/Layout';
 
 const WelcomePage: NextPage = () => {
     return(
-        <Layout theme="light">
+        <Layout theme="dark">
             Welcome page
         </Layout>
         
