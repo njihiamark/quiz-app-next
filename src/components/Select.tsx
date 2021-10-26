12865e7eccae03620647126fa78464c5400bc7de
njihiamark <njihiamark@gmail.com>
2021-10-26 13:26:14 +0300

    select carret and other edits

--- src/components/Select.tsx
+++ src/components/Select.tsx
@@ -1,18 +1,27 @@
 import React from 'react';
+import styles from './input.module.scss';
+import Image from 'next/image';
 
 type Props = {
     iconUrl?: string,
     label?: string,
     options?: string[],
-    placeholder?: string,
+    theme?: string,
     onChange?: () => void,
+    value?: string
 }
 
-function Select() {
+console.log(styles);
+
+function Select({iconUrl="", label="",onChange= () => {}, value="", theme="dark"}: Props) {
     return (
-        <div>
-            
-        </div>
+        <label>
+            <div className={styles["label-container"]}><Image src={iconUrl} width={40} height={40} /> <span className="ml-1">{label}</span> </div>
+            <select value={value} onChange={onChange} className={styles["outline-input"]+' '+styles["custom-select"]+` ${theme === "light" ? styles["outline-input--light"] + " " + styles["custom-select--light"] : styles["outline-input--dark"]+ " " + styles["custom-select--dark"]}`}>
+                <option value="grapefruit">Grapefruit</option>
+                <option value="lime">Lime</option>
+            </select> 
+        </label>
     )
 }
 
