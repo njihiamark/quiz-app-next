f3f65d9598b143dc39d7380983845a17a2a4b61f
njihiamark <njihiamark@gmail.com>
2021-10-26 19:49:48 +0300

    initial progress bar

---
+++ src/components/Progress.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import styles from './progress.module.scss';
+
+type Props = {
+    label?: string,
+    type?: string,
+    theme?: string,
+    onClick?: () => void;
+  }
+
+const Progress = () => (
+    <>
+    <div className={styles["progress-label"]}><span className={styles["status"]}>08</span>/10</div>
+    <div className={styles["progress"]}>
+        <div className={styles["progress-bar"]} style={{width:"80%"}}></div>
+    </div>
+    </>
+    
+);
+
+export default Progress;
