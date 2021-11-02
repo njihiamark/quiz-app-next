88e3b62a5439813a33d5593e190a9976b40e804d
njihiamark <njihiamark@gmail.com>
2021-11-02 19:51:42 +0300

    different outputs for different max_score

--- src/components/Score.tsx
+++ src/components/Score.tsx
@@ -9,25 +9,25 @@ type Props = {
     max_score: number,
 }
 
-const Score = ({page_url = "welcome", score = 8, max_score=10}: Props) => (
-    <div className="mb-1">    
-        <div className={styles["score-info"]+" mb-1"}>
+const Score = ({ page_url = "welcome", score = 8, max_score = 10 }: Props) => (
+    <div className="mb-1">
+        <div className={styles["score-info"] + " mb-1"}>
             <div className={styles["score-info__first-sec"]}>
                 <div className={styles["score-info__avatar"]}>
                     <Image src="/images/avatar.svg" layout="fixed" width={46} height={46} />
                 </div>
-                <span> You scored <span className={styles["score-info__actual"]}>{score}</span>/<span className={styles["score-info__base"]}>{max_score}</span></span>
+                {max_score == 10 ? <span> You scored <span className={styles["score-info__actual"]}>0{score}</span>/<span className={styles["score-info__base"]}>{max_score}</span></span> : <span> You scored <span className={styles["score-info__actual"]}>{Math.round((score/max_score)*100)}%</span></span>}
             </div>
             <div className={styles["score-info__cross"]}>
                 <Link href={page_url}>
                     <Image src="/images/white-cross.svg" width={17} height={17} />
                 </Link>
-            </div>    
+            </div>
         </div>
-        <div className={styles["star-ratings"]+" "+styles["eighth"]}></div>
+        <div className={styles["star-ratings"] + " " + styles["eighth"]}></div>
     </div>
 
-    
+
 );
 
 export default Score;
