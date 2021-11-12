0a8d190c34cc3edf07a7ccc780dd7c444ac2be6b
njihiamark <njihiamark@gmail.com>
2021-11-13 00:47:07 +0300

    initial redirect after entering quiz settings

--- src/pages/play.tsx
+++ src/pages/play.tsx
@@ -1,9 +1,24 @@
 import type { NextPage } from 'next';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+
 import Layout from '../components/Layout';
 import Progress from '../components/Progress';
+import { selectQuestions } from '../features/questions/questionsSlice';
+import { useAppSelector } from '../app/hooks';
+
 
 const PlayPage: NextPage = () => {
-    return(
+    const router = useRouter();
+    const {
+        error
+    } = useAppSelector(selectQuestions);
+    useEffect(() => {
+        if (error) {
+            router.push('/welcome', undefined, { shallow: true })
+        }
+    });
+    return (
         <Layout theme="light"><div>Play Now </div> <Progress current_qn={8} total_qns={10} percentage={80} /></Layout>
     );
 
--- src/pages/welcome.tsx
+++ src/pages/welcome.tsx
@@ -6,8 +6,9 @@ import Input from '../components/Input';
 import Select from '../components/Select';
 import Layout from '../components/Layout';
 import WelcomeHeader from '../components/WelcomeHeader';
-import {useAppDispatch} from '../app/hooks';
-import {getQuestions} from '../features/questions/questionsSlice';
+import { useAppDispatch, useAppSelector } from '../app/hooks';
+import { getQuestions, selectQuestions } from '../features/questions/questionsSlice';
+import { useRouter } from 'next/router';
 
 const WelcomePage: NextPage = () => {
     const [Amount, setAmount] = useState<number>(0);
@@ -16,6 +17,11 @@ const WelcomePage: NextPage = () => {
     const [EmptyAmount, setEmptyAmount] = useState<boolean>(false);
     const [EmptyDifficulty, setEmptyDifficulty] = useState<boolean>(false);
     const dispatch = useAppDispatch();
+    const {
+        pending,
+        error
+    } = useAppSelector(selectQuestions);
+    const router = useRouter();
 
     const handleDifficultyChange = (event) => {
         setEmptyDifficulty(false);
@@ -24,45 +30,50 @@ const WelcomePage: NextPage = () => {
 
     const handleAmountChange = (event) => {
         setEmptyAmount(false);
-        if(event.target.value < 1){
+        if (event.target.value < 1) {
             setAmount(Number(event.target.value = 0));
             setMaxedAmount(false);
-        }else if(event.target.value > 50){
+        } else if (event.target.value > 50) {
             setMaxedAmount(true);
             setAmount(Number(event.target.value = 50));
-        }else{
+        } else {
             setAmount(Number(event.target.value));
             setMaxedAmount(false);
         }
-        
+
     }
 
     const handleSubmit = (event) => {
-        if(Amount <= 0){
+        event.preventDefault();
+        if (Amount <= 0) {
             setEmptyAmount(true);
         }
-        if(Difficulty == ""){
+        if (Difficulty == "") {
             setEmptyDifficulty(true);
         }
-        if(Difficulty == "" || Amount <= 0){
+        if (Difficulty == "" || Amount <= 0) {
             return;
         }
-        dispatch(getQuestions({Amount, Difficulty}));
+        dispatch(getQuestions({ Amount, Difficulty }));
+        router.push('/play', undefined, { shallow: true })
     }
 
     return (
         <Layout theme="dark">
             <div className="w-33-auto">
                 <WelcomeHeader />
-                <Select iconUrl="/images/trophy-icon.svg" label="Difficulty" theme="dark" value={Difficulty} options={["hard", "easy"]} onChange={handleDifficultyChange} />
-                {EmptyDifficulty && <div className="input-message">*Please select an option</div>}
-                <div className="mb-1"></div>
-                <Input type="number" placeholder="Enter amount" iconUrl="/images/star-icon.svg" label="Amount" theme="dark" value={Amount} onChange={handleAmountChange} />
-                {MaxedAmount && <div className="input-message">50 is the maximum amount you can enter</div>}
-                {EmptyAmount && <div className="input-message">*Please input a value greater than 0</div>}
-                <div className="mb-4"></div>
-                <Button label="play" type="filled" theme="dark" onClick={handleSubmit} />
-                <div className="mb-1"></div>
+                <form onSubmit={handleSubmit}>
+                    <Select iconUrl="/images/trophy-icon.svg" label="Difficulty" theme="dark" value={Difficulty} options={["hard", "easy"]} onChange={handleDifficultyChange} />
+                    {EmptyDifficulty && <div className="input-message">*Please select an option</div>}
+                    <div className="mb-1"></div>
+                    <Input type="number" placeholder="Enter amount" iconUrl="/images/star-icon.svg" label="Amount" theme="dark" value={Amount} onChange={handleAmountChange} />
+                    {MaxedAmount && <div className="input-message">50 is the maximum amount you can enter</div>}
+                    {EmptyAmount && <div className="input-message">*Please input a value greater than 0</div>}
+                    <div className="mb-4"></div>
+                    {!pending && <Button label="play" type="filled" theme="dark" />}
+                    {!pending && <div className="mb-1"></div>}
+                    {error && <div>There was an error please try again</div>}
+                </form>
             </div>
         </Layout>
 
