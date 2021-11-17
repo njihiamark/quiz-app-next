f45cf1f8f6d88b3562d52bd36b1b624e817572cd
njihiamark <njihiamark@gmail.com>
2021-11-17 20:26:47 +0300

    correct filename typo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 import Layout from "../components/Layout";
 import WelcomeHeader from "../components/WelcomeHeader";
-import SetQuestions from "../features/questions/setQuestions";
+import SetQuestions from "../features/questions/SetQuestions";
 
 const IndexPage: NextPage = () => {
   return (
