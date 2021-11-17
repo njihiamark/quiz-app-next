1f821979aa30f9003d89990ffb644de7b54b323d
njihiamark <njihiamark@gmail.com>
2021-11-17 19:53:43 +0300

    lean play page

--- src/pages/play.tsx
+++ src/pages/play.tsx
@@ -1,70 +1,16 @@
-import type { NextPage } from 'next';
-import { useEffect, useState, useRef } from 'react';
-import { useRouter } from 'next/router';
-
-import Layout from '../components/Layout';
-import Progress from '../components/Progress';
-import Button from '../components/Button';
-import PlayHeader from '../components/PlayHeader';
-import { selectQuestions, showQuestion } from '../features/questions/questionsSlice';
-import { useAppSelector, useAppDispatch } from '../app/hooks';
-import noDataGohome from '../utils/noDataGoHome';
-
+import type { NextPage } from "next";
+import React from "react";
+import Layout from "../components/Layout";
+import Question from "../features/questions/Question";
 
 const PlayPage: NextPage = () => {
-    const [CurrentQn, setCurrentQn] = useState<number>(1);
-    const [AnsweredQn, setAnsweredQn] = useState<number>(0);
-    const trueBtnRef = useRef(null);
-    const falseBtnRef = useRef(null);
-    const dispatch = useAppDispatch();
-
-    const router = useRouter();
-    const {
-        error,
-        data,
-        pending,
-        difficulty_setting,
-        amount_setting,
-        current_qn,
-        total_qns
-    } = useAppSelector(selectQuestions);
-    useEffect(() => {
-        noDataGohome(error, data, pending, router);
-
-        if (data.length > 0 && !pending) {
-            dispatch(showQuestion({ questionNo: CurrentQn, answer: "" }));
-            trueBtnRef.current.blur();
-            falseBtnRef.current.blur();
-        }
-
-        if (data.length > 0 && AnsweredQn === total_qns) {
-            router.push('/score', undefined, { shallow: true });
-        }
-    }, [pending, CurrentQn, AnsweredQn]);
-
-    const handleAnswering = (answer: string) => {
-        setCurrentQn(CurrentQn + 1);
-        setAnsweredQn(AnsweredQn + 1);
-        dispatch(showQuestion({ questionNo: CurrentQn, answer: answer }));
-    }
-
-    return (
-        <Layout theme="light">
-            <div className="w-33-auto">
-                <PlayHeader difficulty_setting={difficulty_setting} />
-                <div className="mb-2"></div>
-                <Progress current_qn={AnsweredQn} total_qns={amount_setting} qn_text={current_qn?.question} />
-                {data.length > 0 && !pending ? <div>
-                    <Button label="true" type="filled" theme="light" onClick={() => handleAnswering("True")} refValue={trueBtnRef} />
-                    <div className="mb-2"></div>
-                    <Button label="false" type="outline" theme="light" onClick={() => handleAnswering("False")} refValue={falseBtnRef} />
-                    <div className="mb-1"></div>
-                </div> : <div>loading...</div>}
-
-            </div>
-        </Layout>
-    );
-
-}
+  return (
+    <Layout theme="light">
+      <div className="w-33-auto">
+        <Question />
+      </div>
+    </Layout>
+  );
+};
 
 export default PlayPage;
--- src/pages/welcome.tsx
+++ src/pages/welcome.tsx
@@ -1,13 +1,11 @@
 import type { NextPage } from "next";
-import React, { useState } from "react";
+import React from "react";
 import Layout from "../components/Layout";
 
 import WelcomeHeader from "../components/WelcomeHeader";
 import SetQuestions from "../features/questions/setQuestions";
 
-
 const WelcomePage: NextPage = () => {
-  
   return (
     <Layout theme="dark">
       <div className="w-33-auto">
