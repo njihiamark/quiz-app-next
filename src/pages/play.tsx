7d42d314f8d98f2ded76071b38aa5514bb07d13f
njihiamark <njihiamark@gmail.com>
2021-11-17 17:18:35 +0300

    add focus and blur for play btns

--- src/pages/play.tsx
+++ src/pages/play.tsx
@@ -1,5 +1,5 @@
 import type { NextPage } from 'next';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import { useRouter } from 'next/router';
 
 import Layout from '../components/Layout';
@@ -14,6 +14,8 @@ import noDataGohome from '../utils/noDataGoHome';
 const PlayPage: NextPage = () => {
     const [CurrentQn, setCurrentQn] = useState<number>(1);
     const [AnsweredQn, setAnsweredQn] = useState<number>(0);
+    const trueBtnRef = useRef(null);
+    const falseBtnRef = useRef(null);
     const dispatch = useAppDispatch();
 
     const router = useRouter();
@@ -28,16 +30,19 @@ const PlayPage: NextPage = () => {
     } = useAppSelector(selectQuestions);
     useEffect(() => {
         noDataGohome(error, data, pending, router);
+
         if (data.length > 0 && !pending) {
             dispatch(showQuestion({ questionNo: CurrentQn, answer: "" }));
+            trueBtnRef.current.blur();
+            falseBtnRef.current.blur();
         }
-        
-        if(data.length > 0 && AnsweredQn === total_qns){
+
+        if (data.length > 0 && AnsweredQn === total_qns) {
             router.push('/score', undefined, { shallow: true });
         }
     }, [pending, CurrentQn, AnsweredQn]);
 
-    const handleAnswering = (answer:string) => {
+    const handleAnswering = (answer: string) => {
         setCurrentQn(CurrentQn + 1);
         setAnsweredQn(AnsweredQn + 1);
         dispatch(showQuestion({ questionNo: CurrentQn, answer: answer }));
@@ -50,11 +55,11 @@ const PlayPage: NextPage = () => {
                 <div className="mb-2"></div>
                 <Progress current_qn={AnsweredQn} total_qns={amount_setting} qn_text={current_qn?.question} />
                 {data.length > 0 && !pending ? <div>
-                    <Button label="true" type="filled" theme="light" onClick={() => handleAnswering("True")} />
+                    <Button label="true" type="filled" theme="light" onClick={() => handleAnswering("True")} refValue={trueBtnRef} />
                     <div className="mb-2"></div>
-                    <Button label="false" type="outline" theme="light" onClick={() => handleAnswering("False")} />
+                    <Button label="false" type="outline" theme="light" onClick={() => handleAnswering("False")} refValue={falseBtnRef} />
                     <div className="mb-1"></div>
-                </div>: <div>loading...</div>}
+                </div> : <div>loading...</div>}
 
             </div>
         </Layout>
